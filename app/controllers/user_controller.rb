0cf395bb55c46e76adafb714b66ab063aec45ac3
young moon <young.utf@gmail.com>
2014-10-04 07:46:33 +0000

    회우너가입 정보 저장 및 화면 전환 기능 추가하기

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,24 @@
 class UserController < ApplicationController
   def signup
   end
+
+
+	def signup_complete
+		u = User.new
+		u.username = params[:username]
+		if params[:password] == params[:retype_password]
+			u.password = params[:password]
+			if u.save
+				flash[:alert] = "Successfully signed up."
+				redirect_to "/wall/posts"
+			else
+				flash[:alert] = u.error.values.flatten.join(' ')
+				redirect_to :back
+			end
+		else
+			flash[:alert] = "Password doesn't match."
+			redirect_to :back
+		end
+	end
+
 end
